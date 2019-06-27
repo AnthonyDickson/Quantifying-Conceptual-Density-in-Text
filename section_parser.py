21b7fa2b709b5dd6f1c4634bd010c686c3d58883
Anthony Dickson <eight0153@gmail.com>
2019-06-28 10:06:16 +1200

    implement basic graph

--- section_parser.py
+++ section_parser.py
@@ -6,10 +6,129 @@ import graphviz
 import nltk
 
 
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        self.node_index = dict()
+        self.section_index = dict()
+        self.section_nodes = set()
+        self.edges = dict()
+
+    def add_node(self, node):
+        if node.name == node.section_name and node in self.nodes:
+            prev_section_name = self.node_index[node.name].section_name
+            self.node_index[node.name].section_name = node.section_name
+            self.section_nodes.add(node)
+
+            if prev_section_name in self.section_index:
+                self.section_index[prev_section_name].remove(node)
+
+            if node.section_name in self.section_index:
+                self.section_index[node.section_name].add(node)
+            else:
+                self.section_index[node.section_name] = {node}
+
+            return
+        elif node in self.nodes:
+            return
+
+        self.nodes.add(node)
+        self.node_index[node.name] = node
+
+        if node.section_name in self.section_index:
+            self.section_index[node.section_name].add(node)
+        else:
+            self.section_index[node.section_name] = {node}
+
+        if node.name == node.section_name:
+            self.section_nodes.add(node)
+
+    def remove_node(self, node):
+        self.nodes.remove(node)
+        del self.node_index[node.name]
+        self.section_nodes.remove(node)
+        self.section_index[node.section_name].remove(node)
+
+    def add_edge(self, tail, head):
+        if tail == head:
+            return
+        elif tail in self.edges:
+            self.edges[tail].add(head)
+        else:
+            self.edges[tail] = {head}
+
+    def render(self):
+        try:
+            g = graphviz.Digraph(engine='circo')
+
+            with g.subgraph(name='cluster_main_entities') as sg:
+                sg.attr('node', shape='doublecircle')
+
+                for section_node in self.section_nodes:
+                    sg.node(section_node.name)
+
+            for node in self.nodes:
+                g.node(node.name)
+
+            for tail in self.edges:
+                for head in self.edges[tail]:
+                    g.edge(tail.name, head.name)
+
+            g.render(format='png', view=True)
+        except graphviz.backend.ExecutableNotFound:
+            print('Could not display graph -- GraphViz does not seem to be installed.')
+
+
+class Node:
+    def __init__(self, name, section_name):
+        self.name = name
+        self.section_name = section_name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return 'Node(%s, %s)' % (self.name, self.section_name)
+
+
+class Edge:
+    def __init__(self, tail, head, weight=1):
+        self.tail = tail
+        self.head = head
+        self.weight = weight
+        self.color = 'black'
+
+    def render(self, g):
+        g.edge(self.tail.name, self.head.name,
+               penwidth=str(1 + log2(self.weight)),
+               color=self.color)
+
+
+class ForwardEdge(Edge):
+    def __init__(self, tail, head, weight=1):
+        super().__init__(tail, head, weight)
+
+        self.color = 'blue'
+
+
+class BackwardEdge(Edge):
+    def __init__(self, tail, head, weight=1):
+        super().__init__(tail, head, weight)
+
+        self.color = 'red'
+
+
 class Chunk:
     """A chunk is a section of text on a certain topic.
     It keeps track of what things are related to it.
     """
+
     def __init__(self, name):
         self.name = name
         self.entity_counts = dict()
@@ -78,6 +197,7 @@ def register_entity(entity, chunk, entities):
         entities[entity] = {chunk.name}
 
 
+# TODO: Fix bugs with single letter entities
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse a file and group the entities by section.')
     parser.add_argument('-f', '--file', metavar='INPUT_FILE', type=str, required=True,
@@ -102,6 +222,7 @@ if __name__ == '__main__':
                     {<NBAR>}
             """
     chunker = nltk.RegexpParser(grammar)
+    graph = Graph()
 
     for section in root.findall('section'):
         section_title = section.find('title').text
@@ -109,6 +230,8 @@ if __name__ == '__main__':
 
         chunk = Chunk(section_title)
         chunks_dict[section_title] = chunk
+
+        graph.add_node(Node(section_title, section_title))
 
         if chunk.name in entities:
             entities[chunk.name].add(chunk.name)
@@ -119,6 +242,8 @@ if __name__ == '__main__':
             entity_name = entity.text.lower()
 
             register_entity(entity_name, chunk, entities)
+            graph.add_node(Node(entity_name, section_title))
+            graph.add_edge(graph.node_index[section_title], graph.node_index[entity_name])
 
             # register permutations of a phrase.
             # E.g. 'wheat flour' gives the entities 'wheat', 'flour', and 'wheat flour'
@@ -128,16 +253,24 @@ if __name__ == '__main__':
             tags = nltk.pos_tag(phrase)
             tree = chunker.parse(tags)
 
-            register_entity(entity_name, chunk, entities)
-
             for st in tree.subtrees(filter=lambda t: t.label() == 'NBAR'):
-                register_entity(' '.join([str(token) for token, tag in st]), chunk, entities)
+                nbar = ' '.join([str(token) for token, tag in st])
+                register_entity(nbar, chunk, entities)
+
+                graph.add_node(Node(nbar, section_title))
+                graph.add_edge(graph.node_index[entity_name], graph.node_index[nbar])
 
                 nouns = [token for token, tag in st if tag.startswith('NN')]
 
                 for i in range(len(nouns)):
                     register_entity(nouns[i], chunk, entities)
-                    register_entity(' '.join(nouns[i:]), chunk, entities)
+                    graph.add_node(Node(nouns[i], section_title))
+                    graph.add_edge(graph.node_index[nbar], graph.node_index[nouns[i]])
+
+                    substr = ' '.join(nouns[i:])
+                    register_entity(substr, chunk, entities)
+                    graph.add_node(Node(substr, section_title))
+                    graph.add_edge(graph.node_index[nbar], graph.node_index[substr])
 
         chunks.append(chunk)
 
@@ -156,59 +289,62 @@ if __name__ == '__main__':
         print('Found %d cycle(s) in the graph.' % n_cycles)
         print('These nodes are part of at least one cycle:', nodes_in_cycles)
 
-    # Display the graph representing the relationships of the entities in the text.
-    try:
-        g = graphviz.Digraph(engine='circo')
+    # TODO: do the below except with `graph`.
+    # # Display the graph representing the relationships of the entities in the text.
+    # try:
+    #     g = graphviz.Digraph(engine='circo')
+    #
+    #     # Add the nodes for the sections first
+    #     with g.subgraph(name='cluster_main_entities') as sg:
+    #         sg.attr('node', shape='doublecircle')
+    #
+    #         for chunk in chunks:
+    #             sg.node(chunk.name)
+    #
+    #     # Add the nodes for the entities and all of the edges
+    #     forward_backward_links = set()
+    #
+    #     for i, chunk in enumerate(chunks):
+    #         print('Chunk: %s' % chunk)
+    #
+    #         # Check for forward and backward links between chunk[i] and chunk[j]
+    #         for j in range(i + 1, len(chunks)):
+    #             # Check for forward links
+    #             for entity in chunk.entity_counts:
+    #                 if entity in chunks[j].name:
+    #                     print("Forward link from '%s' to '%s'" % (chunk.name, chunks[j].name))
+    #                     g.edge(chunk.name, entity,
+    #                            penwidth=str(chunk.log_count(entity)),
+    #                            color='blue')
+    #                     forward_backward_links.add((chunk.name, entity))
+    #
+    #             # Check for backward links
+    #             for entity in chunks[j].entity_counts:
+    #                 if entity in chunk.name:
+    #                     print("Backward link from '%s' to '%s'" % (chunks[j].name, chunk.name))
+    #                     g.edge(chunks[j].name, entity,
+    #                            penwidth=str(chunks[j].log_count(entity)),
+    #                            color='red')
+    #                     forward_backward_links.add((chunks[j].name, entity))
+    #
+    #         # Check for self-contained and shared entities, also add links
+    #         # that are neither forward or backward.
+    #         for entity in chunk.entity_counts:
+    #             if chunk.name in entities[entity]:
+    #                 if len(entities[entity]) == 1:
+    #                     print("Self-contained entity '%s' found in: '%s'." % (entity, chunk.name))
+    #                 else:
+    #                     print("Shared entity '%s' found in %s." % (entity, ', '.join(entities[entity])))
+    #
+    #             # Do check to avoid adding duplicate links
+    #             if (chunk.name, entity) not in forward_backward_links:
+    #                 g.edge(chunk.name, entity,
+    #                        penwidth=str(chunk.log_count(entity)))
+    #
+    #         print()
+    #
+    #     g.render(format='png', view=True)
+    # except graphviz.backend.ExecutableNotFound:
+    #     print('Could not display graph -- GraphViz does not seem to be installed.')
 
-        # Add the nodes for the sections first
-        with g.subgraph(name='cluster_main_entities') as sg:
-            sg.attr('node', shape='doublecircle')
-
-            for chunk in chunks:
-                sg.node(chunk.name)
-
-        # Add the nodes for the entities and all of the edges
-        forward_backward_links = set()
-
-        for i, chunk in enumerate(chunks):
-            print('Chunk: %s' % chunk)
-
-            # Check for forward and backward links between chunk[i] and chunk[j]
-            for j in range(i + 1, len(chunks)):
-                # Check for forward links
-                for entity in chunk.entity_counts:
-                    if entity in chunks[j].name:
-                        print("Forward link from '%s' to '%s'" % (chunk.name, chunks[j].name))
-                        g.edge(chunk.name, entity,
-                               penwidth=str(chunk.log_count(entity)),
-                               color='blue')
-                        forward_backward_links.add((chunk.name, entity))
-
-                # Check for backward links
-                for entity in chunks[j].entity_counts:
-                    if entity in chunk.name:
-                        print("Backward link from '%s' to '%s'" % (chunks[j].name, chunk.name))
-                        g.edge(chunks[j].name, entity,
-                               penwidth=str(chunks[j].log_count(entity)),
-                               color='red')
-                        forward_backward_links.add((chunks[j].name, entity))
-
-            # Check for self-contained and shared entities, also add links
-            # that are neither forward or backward.
-            for entity in chunk.entity_counts:
-                if chunk.name in entities[entity]:
-                    if len(entities[entity]) == 1:
-                        print("Self-contained entity '%s' found in: '%s'." % (entity, chunk.name))
-                    else:
-                        print("Shared entity '%s' found in %s." % (entity, ', '.join(entities[entity])))
-
-                # Do check to avoid adding duplicate links
-                if (chunk.name, entity) not in forward_backward_links:
-                    g.edge(chunk.name, entity,
-                           penwidth=str(chunk.log_count(entity)))
-
-            print()
-
-        g.render(format='png', view=True)
-    except graphviz.backend.ExecutableNotFound:
-        print('Could not display graph -- GraphViz does not seem to be installed.')
+    graph.render()
