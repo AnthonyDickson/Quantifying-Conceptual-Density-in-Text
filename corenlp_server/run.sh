2d3d9f9ec59464e868a3d054bac21db1b225f542
Anthony Dickson <eight0153@gmail.com>
2019-07-17 17:13:28 +1200

    bring default props in line with default load list for CoreNLP server

--- corenlp_server/run.sh
+++ corenlp_server/run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+maxMemory=2 # in GB
+port=9000
+timeout=30000 # in milliseconds
+threads=6
+maxCharLength=10000
+serverProperties="default.props"
+preloadList="tokenize,ssplit,pos,lemma,parse,natlog,depparse,openie"
+
+
 # Argument parsing adapted from https://stackoverflow.com/a/5476278
 usage="$(basename "$0") [-h] [-a s] [-c n] [-m n] [-o n] [-p n] [-s s] [-t n]
     -- Run an Stanford CoreNLP server locally.
@@ -7,26 +16,17 @@ usage="$(basename "$0") [-h] [-a s] [-c n] [-m n] [-o n] [-p n] [-s s] [-t n]
 where:
     -h              Show this help text
     -a ANNOTATORS   The annotators to preload into cache (default:
-                        \"tokenize,ssplit,pos,parse,depparse,openie\")
-    -c MAX_CHARS    The maximum text length in characters (default: 10000)
+                        \"${preloadList}\")
+    -c MAX_CHARS    The maximum text length in characters (default: ${maxCharLength})
     -m MAX_MEMORY   The maximum amount of memory to allocate to the JVM in GB
-                        (default: 2)
-    -o TIMEOUT      The timeout in milliseconds (default: 30000)
-    -p PORT         The port to run the server on (default: 9000)
-    -s SERVER_PROPS The server properties file to use (default:
-                        \"default.props\")
-    -t N_THREADS    The number of threads to use (default: 6) See
+                        (default: ${maxMemory})
+    -o TIMEOUT      The timeout in milliseconds (default: ${timeout})
+    -p PORT         The port to run the server on (default: ${port})
+    -s SERVER_PROPS The server properties file to use (default: \"${serverProperties}\")
+    -t N_THREADS    The number of threads to use (default: ${threads}) See
                         https://stanfordnlp.github.io/CoreNLP/cmdline.html#configuring-corenlp-properties
                         for more details.
 "
-
-maxMemory=2 # in GB
-port=9000
-timeout=30000 # in milliseconds
-threads=6
-maxCharLength=10000
-serverProperties="default.props"
-preloadList="tokenize,ssplit,pos,parse,depparse,openie"
 
 while getopts ':ht:j::e' option; do
   case "$option" in
